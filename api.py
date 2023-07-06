c2d6c06e315878b99df122ccd10c1c5fcfe2b8ec
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2023-07-06 21:56:49 +0000

    Heavily overhaul api.py
    Changelog:
    - Now uses waitress to help with performance (Can be disabled with either debug=true or use_waitress=false)
    - No longer loads the dataset on every run. Should help with performance
    - Disable the webui until further notice (It sucks, development on a discord bot is easier lol)
    - Now takes in a username parameter which is used by openai to monitor abuse
    - More robust error handling

--- api.py
+++ api.py
@@ -1,20 +1,26 @@
 import flask
-from main import answer_question
+from main import answer_question, df
 import pandas as pd
 import numpy as np
-import werkzeug.exceptions
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import os
+from distutils.util import strtobool
+from waitress import serve
+import traceback
+import logging
 
 # Load Flask
 def get_proxy_remote_address():
-	"""
-	:return: the ip address for the current request (or 127.0.0.1 if none found)
-	"""
-	if flask.request.headers.get('X-Forwarded-For') != None:
-		return str(flask.request.headers.get('X-Forwarded-For'))
-	return flask.request.remote_addr or '127.0.0.1'
+    """
+    :return: the ip address for the current request (or 127.0.0.1 if none found)
+    """
+    if flask.request.headers.get("X-Forwarded-For") != None:
+        return str(flask.request.headers.get("X-Forwarded-For"))
+    return flask.request.remote_addr or "127.0.0.1"
 
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 app = flask.Flask(__name__)
 limiter = Limiter(
     get_proxy_remote_address,
@@ -22,25 +28,55 @@ limiter = Limiter(
     storage_uri="memory://",
 )
 
+# Check if the dataset is set in the environment variables
+if not os.getenv("DATASET"):
+    raise Exception("Please set the DATASET environment variable")
 
-# Load the data
-df=pd.read_csv('processed/embeddings.csv', index_col=0)
-df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
-df.head()
+dataset = str(os.getenv("DATASET"))
+
 
 # Create the API to answer questions
-@app.route('/api/v1/ask', methods=['POST'])
+@app.route("/api/v1/ask", methods=["POST"])
 @limiter.limit("10/30 seconds")
 def ask():
-    question = flask.request.json['prompt']
-#    print(question + " - " + flask.request.headers['X-Forwarded-For'])
-    print(question + " - " + get_proxy_remote_address())
-    answer = answer_question(df, question=question, debug=True)
-    print(answer)
-    return flask.jsonify(answer)
-    
+    data = flask.request.get_json()
+
+    if data is not None:
+        question = data["prompt"]
+
+        if question is not None:
+            print(question + " - " + get_proxy_remote_address())
+
+            username = data["username"]
+
+            # If the username is provided, use it to answer the question
+            if username is not None:
+                try:
+                    answer = answer_question(
+                        df, question=question, username=username, debug=True
+                    )
+                    return flask.jsonify(answer), 200
+                except Exception as e:
+                    print(traceback.format_exc(), flush=True)
+                    return flask.jsonify({"error": str(e)}), 500
+            else:
+                # Otherwise, answer the question without a username
+                try:
+                    answer = answer_question(df, question=question, debug=True)
+                    return flask.jsonify(answer), 200
+                except Exception as e:
+                    print(traceback.format_exc(), flush=True)
+                    return flask.jsonify({"error": str(e)}), 500
+
+
+        else:
+            return flask.jsonify({"error": "No prompt"}), 400
+    else:
+        return flask.jsonify({"error": "No data"}), 400
+
+
 # Create the Web UI
-@app.route('/<path:path>')
+""" @app.route('/<path:path>')
 def index(path):
     print(path + "index.html")
     try:
@@ -48,7 +84,24 @@ def index(path):
     except werkzeug.exceptions.NotFound as e:
         if path.endswith("/"):
             return flask.send_from_directory('ui', path + "index.html")
-        raise e
+        raise e """
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3636, debug=True)
+if __name__ == "__main__":
+    debug = os.environ.get("DEBUG", True)
+    use_waitress = os.getenv("USE_WAITRESS", False)
+    print("Debug: " + str(debug))
+
+    if debug is not bool:
+        debug = strtobool(debug)
+        
+    if use_waitress is not bool:
+        use_waitress = strtobool(use_waitress)
+
+    if debug == True and not use_waitress:
+        app.run(host="0.0.0.0", port=3636, debug=True)
+        print("Started with flask", flush=True)
+    else:
+        logger = logging.getLogger("waitress")
+        logger.setLevel(logging.INFO)
+        serve(app, host="0.0.0.0", port=3636)
+        print("Started with waitress", flush=True)
