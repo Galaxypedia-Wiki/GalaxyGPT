07c014236069291142c6295528a3ee34b1ba8b41
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-01-05 17:24:14 -0500

    ruff linting and formatting

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import threading
 import time
 import traceback
 import warnings
-from distutils.util import strtobool
 
 import colorama
 import numpy as np
@@ -23,7 +22,7 @@ from scipy.spatial.distance import cosine
 load_dotenv()
 
 GalaxyGPTVersion = os.getenv("VERSION")
-if GalaxyGPTVersion == None:
+if GalaxyGPTVersion is None:
     raise Exception("Please set VERSION in .env")
 
 if not os.getenv("OPENAI_ORG_ID") or not os.getenv("OPENAI_API_KEY"):
@@ -37,13 +36,13 @@ openai_client = OpenAI(
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 dataset = str(os.getenv("DATASET")) # Dataset to use
-if dataset == None:
+if dataset is None:
     raise Exception("Please set DATASET in .env")
 context_len = int(os.getenv("MAX_CONTEXT_LEN")) # Maximum context length in tokens
-if context_len == None:
+if context_len is None:
     raise Exception("Please set MAX_CONTEXT_LEN in .env")
 gpt_model = str(os.getenv("MODEL")) # Model to use
-if gpt_model == None:
+if gpt_model is None:
     raise Exception("Please set MODEL in .env")
 
 print("GalaxyGPT v" + GalaxyGPTVersion + " - " + dataset + " - " + str(context_len) + " max len")
@@ -73,9 +72,9 @@ def strtobool(string: str):
     False values are 'n', 'no', 'f', 'false', 'off', and '0'.  
     Raises ValueError if 'string' is anything else.
     """
-    if string.lower() in ['y', 'yes', 't', 'true', 'on', '1']: 
+    if string.lower() in ["y", "yes", "t", "true", "on", "1"]: 
         return True
-    elif string.lower() in ['n', 'no', 'f', 'false', 'off', '0']: 
+    elif string.lower() in ["n", "no", "f", "false", "off", "0"]: 
         return False
     else: 
         raise ValueError(f"invalid string given: {string}")
@@ -112,7 +111,7 @@ def create_context(question, df, max_len=context_len, model="text-embedding-ada-
     if debug:
         print("bingus:", flush=True)
         print(returns, flush=True)
-        print('-------------------', flush=True)
+        print("-------------------", flush=True)
         print(df["distances"].values.tolist(), flush=True)
 
     # Return the context
@@ -278,7 +277,7 @@ class ADCS:
     @staticmethod
     def createDataset(reload=False, webhook=None, noembeddings=False):
         global df, dataset
-        if os.getenv("DATABASE_PASSWORD") == None:
+        if os.getenv("DATABASE_PASSWORD") is None:
             raise Exception("Please set DATABASE_PASSWORD in .env")
         
         # Generate the dataset
@@ -287,13 +286,13 @@ class ADCS:
         
         print(colorama.Fore.CYAN + "ADCS:" + colorama.Fore.RESET + " Preparing the dataset...")
         # Prepare the dataset
-        if noembeddings == True:
+        if noembeddings is True:
             print("Won't be generating embeddings for this dataset")
             subprocess.run(["python3", "dataset.py", "-o", "dataset-ADCS", "--max-len", str(int(context_len/2)), "--no-embeddings", "--cleandir"], cwd=os.path.join(__location__))
         else:
             subprocess.run(["python3", "dataset.py", "-o", "dataset-ADCS", "--max-len", str(int(context_len/2)), "--cleandir"], cwd=os.path.join(__location__))
 
-        if reload == True and noembeddings == False:
+        if reload is True and noembeddings is False:
             ADCS.reloadDataset("dataset-ADCS")
             
         print("Dataset created!")
@@ -312,7 +311,7 @@ class ADCS:
         ADCS.timer = schedule.every().day.at("00:00").do(ADCS.createDataset, True)
         
         def loop():
-            while ADCS.timerbreak == False:
+            while ADCS.timerbreak is False:
                 schedule.run_pending()
                 time.sleep(1)
             print(colorama.Fore.CYAN + "ADCS:" + colorama.Fore.RESET + " Stopped!")
@@ -339,11 +338,11 @@ class ADCS:
             ADCS.webhook.execute()
 
 adcsdefault = strtobool(os.getenv("ADCS", "False"))
-if adcsdefault == True:
+if adcsdefault is True:
         scheduler = ADCS()
         if scheduler.status == "Stopped":
             scheduler.start()
-elif adcsdefault == False:
+elif adcsdefault is False:
     print("ADCS is currently disabled")            
 
 if __name__ == "__main__":
