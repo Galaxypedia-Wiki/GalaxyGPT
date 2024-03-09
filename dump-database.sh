cf0ef7dfe2822d492f8a0b4a7d9edf15986cdb32
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-03-08 22:36:17 -0500

    Update database dump to include namespace information

--- dump-database.sh
+++ dump-database.sh
@@ -5,7 +5,7 @@ set -e
 
 # Dump the sql database to a csv file
 echo "Dumping the sql database to a csv file..."
-mysql -u root -p$1 -e "USE galaxypedia; SELECT page_title \"page_name\", old_text \"content\" FROM page INNER JOIN slots on page_latest = slot_revision_id INNER JOIN slot_roles on slot_role_id = role_id AND role_name = 'main' INNER JOIN content on slot_content_id = content_id INNER JOIN text on substring( content_address, 4 ) = old_id AND left( content_address, 3 ) = \"tt:\" WHERE (page.page_namespace = 0 OR page.page_namespace = 4) AND page.page_is_redirect = 0 into outfile '/tmp/galaxypedia.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
+mysql -u root -p$1 -e "USE galaxypedia; SELECT page_namespace, page_title \"page_name\", old_text \"content\" FROM page INNER JOIN slots on page_latest = slot_revision_id INNER JOIN slot_roles on slot_role_id = role_id AND role_name = 'main' INNER JOIN content on slot_content_id = content_id INNER JOIN text on substring( content_address, 4 ) = old_id AND left( content_address, 3 ) = \"tt:\" WHERE (page.page_namespace = 0 OR page.page_namespace = 4) AND page.page_is_redirect = 0 into outfile '/tmp/galaxypedia.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
 
 # Move the csv file to the current directory
 echo "Moving the csv file to the current directory..."
@@ -17,7 +17,7 @@ sudo chown $(whoami):$(whoami) galaxypedia.csv.temp
 
 # Add the header to the csv file
 echo "Adding the header to the csv file..."
-(echo "page_title,content"; cat galaxypedia.csv.temp) > galaxypedia-$(date '+%Y-%m-%d').csv
+(echo "page_namespace, page_title,content"; cat galaxypedia.csv.temp) > galaxypedia-$(date '+%Y-%m-%d').csv
 
 # Remove the temporary file
 echo "Removing the temporary file..."
